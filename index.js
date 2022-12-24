5074f4899e3d421ec6901a2e0d8ac3ab4d090251
Eugene Kapelko <evgeniy.kapelko.c@balanceinternet.com.au>
2022-12-24 21:52:57 +0200

    Work with routing

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { userRouter } from './users/users.js';
+
+const port = 8001;
+const app = express();
+
+//app.all('/')
+app.get('/hello', (req, res) => {
+    res.send('Hey from express!')
+});
+
+app.use('/users', userRouter);
+
+app.listen(port, () => {
+    console.log(`Server run on http://localhost:${port}`)
+
+})
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hey!');
+// });
+
+// server.listen(port, host, () => {
+//     console.log(`Server run on ${host}:${port}`)
+// });
