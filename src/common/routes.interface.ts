2b0edd00f0f039339b3a86b345757a35572ccd03
Eugene Kapelko <evgeniy.kapelko.c@balanceinternet.com.au>
2022-12-31 19:32:34 +0200

    Create Middleware for routes

--- src/common/routes.interface.ts
+++ src/common/routes.interface.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction, Router } from 'express';
+import { IMiddleware } from './middleware.interface';
+
 export interface IControllerRoute {
     path: string;
     func: (req: Request, res: Response, next: NextFunction) => void;
-    method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>
+    method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
+    middlewares?: IMiddleware[]
 }
